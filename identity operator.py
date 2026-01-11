f3017cf6d7cdc0a8c150b8253fcbfdcf9bba27ff
cleo581 <fclive7@gmail.com>
2026-01-11 16:06:12 +0000

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,13 @@
+print("identity operator ")
+x=5
+if type(x) is int:
+    print("x is an interger")
+else:
+    print("x is not an interger") 
+y=2.56   
+if type(y) is not float:
+    print("true")
+else:
+    print("false")
+    
+    
