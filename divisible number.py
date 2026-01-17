51bdb50246d4e5020bdec4a2d5b1d8a5e53ea866
cleo581 <fclive7@gmail.com>
2026-01-17 15:58:41 +0000

    Add files via upload

---
+++ divisible number.py
@@ -0,0 +1,9 @@
+print("divisible number")
+print("enter a numerator")
+num=int(input())
+print("enter a denominator")
+den=int(input())
+if num%den==0:
+    print(" numerator is divisble ")
+else:
+    print("numerator is not divisble")
