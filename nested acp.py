8b1cecdcbdb932fc2da0e77ee3035870697386d3
cleo581 <fclive7@gmail.com>
2026-02-01 15:56:14 +0000

    Add files via upload

---
+++ nested acp.py
@@ -0,0 +1,11 @@
+print("we have to check how many times a character is repeated in a word")
+word=input("enter a word")
+char=input("enter a charchter to search")
+i=0
+count=0
+while i<len(word):
+    if word[i]==char:
+        count=count+1
+    i=i+1
+print("the total number of your charcter is ",count)
+        
