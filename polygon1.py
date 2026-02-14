bb27edc1331d9b0f589281e3ccbca93ad06b00dc
cleo581 <fclive7@gmail.com>
2026-02-14 16:00:20 +0000

    Add files via upload

---
+++ polygon1.py
@@ -0,0 +1,11 @@
+import turtle 
+turtle.Screen.bgcolor("orange")
+turtle.Screen.setup(400,400)
+polygon=turtle.Turtle()
+num_sides=6
+side_lenght=70
+angle=360/num_sides
+for i in range(num_sides):
+    polygon.forward(side_lenght)
+    polygon.right(angle)
+turtle.done()
