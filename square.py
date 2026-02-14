4e084783c2347ab7ec87961750b8d3374163586b
cleo581 <fclive7@gmail.com>
2026-02-14 17:18:58 +0000

    Add files via upload

---
+++ square.py
@@ -0,0 +1,21 @@
+from email.mime import image
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(400,400)
+python=turtle.Turtle()
+num_sides=4
+side_lenght=50
+angle=360/num_sides
+for i in range(num_sides):
+     "polygon".forward(side_lenght)
+     "polygon".right(angle)
+"turtle_done"()
+
+  # pyright: ignore[reportUndefinedVariable]
+ # type: ignore
+# pyright: ignore[reportUndefinedVariable]
+
+
+
+
+
