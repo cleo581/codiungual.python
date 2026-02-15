a801fa87180cae864038cc3a127ba82c67c8534e
cleo581 <fclive7@gmail.com>
2026-02-15 18:12:05 +0000

    Add files via upload

---
+++ define.py
@@ -0,0 +1,20 @@
+import math
+def circumfrience(radius):
+    return   math.pi*radius
+    r=float(input("enter radius"))
+    c=circumfrience(r)
+    print("the circumfirence is:",c)
+    
+
+ 
+
+
+
+
+
+
+    
+
+
+    
+
