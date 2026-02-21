94c0f6ca312ed35128c5e742809aafc3505a98d0
cleo581 <fclive7@gmail.com>
2026-02-21 16:48:42 +0000

    Add files via upload

---
+++ shutsown.py
@@ -0,0 +1,9 @@
+def shutdown():
+     print("enter two numbers")
+user_input = input("Enter 'yes' or 'no': ")
+if user_input == "yes":
+        print("not shutting down")
+elif user_input == "no":
+        print("shutting down")
+else:
+        print("sorrry")
