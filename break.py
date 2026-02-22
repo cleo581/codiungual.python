edc43919519d75dc3e9711c101d45503f3ffa98f
cleo581 <fclive7@gmail.com>
2026-02-22 15:52:58 +0000

    Add files via upload

---
+++ break.py
@@ -0,0 +1,14 @@
+a=input("enter a word: ")
+for i in a:
+    if (i=="a"):
+        print("a is found")
+        break
+    else:
+        print("a is not found")
+         
+for i in range(10):
+    if i==5:
+        continue
+    print(i)
+
+        
