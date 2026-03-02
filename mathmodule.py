6d75741979dab54508c0ba7fb860a5853f0e304c
cleo581 <fclive7@gmail.com>
2026-03-02 18:35:38 +0000

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,16 @@
+import math
+print("welcome to maths functions")
+print("floor and celing value of 23.78")
+print(str(math.ceil(172.2)))
+print(str(math.floor(67.87)))
+
+x=10
+y=-20
+print("the value of x after copying the sign of y "+ str(math.copysign(x,y)))
+
+print("find the gcd of 24 and 56")
+print("the gcd of 24 and 56 is= "+str(math.gcd(24,56)))
+
+
+
+
