63f941f1ee6260806ad36872e936f14c0ea1ceec
cleo581 <fclive7@gmail.com>
2026-03-13 17:49:09 +0000

    Add files via upload

---
+++ sqyare.py
@@ -0,0 +1,49 @@
+import math
+from tracemalloc import start
+def square_filter(start,end):
+ even_squares = {}
+odd_squares = {}
+even_squares = {}
+for i in range(start, "end"+1):
+       square= i**2
+       if square%2==0:
+              even_squares[i]=square
+       else: 
+                odd_squares[i]=square
+                print("odd  values are :",odd_squares)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
