c996f88bcbd464879245820c2a5eed743db1d3b8
cleo581 <fclive7@gmail.com>
2026-03-15 16:24:09 +0000

    Add files via upload

---
+++ flipflop.py
@@ -0,0 +1,17 @@
+def palin(r):
+    e=len(r)-1
+    s=0
+    while s<e:
+        if(r[s]!=r[e]):
+            return False
+        s=s+1
+        e=e-1
+    return True
+r=(1,2,3,3,2,1,)
+if(palin(r)):
+   print("the tuple is flip flop")
+else:
+    print("the tuple is not flip flop")
+
+
+
