9c0d0f7081b868278eb56f4eb918d4886a4bfa3e
cleo581 <fclive7@gmail.com>
2026-03-28 15:52:20 +0000

    Add files via upload

---
+++ set.py
@@ -0,0 +1,14 @@
+set_1={3,3.5,"hello"}
+print(set_1)
+
+set2={1,2,3,2,3,5}
+print(set2)
+
+set3=set([1,2,3,2])
+print(set3)
+
+set4=set([0,1,3,4,5])
+set4.pop()
+print(set4)
+set4.add(9)
+print(set4)
