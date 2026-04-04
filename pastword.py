dabf54461d3cbd5d60b021065a478d0d30c655f0
cleo581 <fclive7@gmail.com>
2026-04-04 19:13:08 +0100

    Add files via upload

---
+++ pastword.py
@@ -0,0 +1,6 @@
+
+import random
+print("well come to the the password game")
+def password():
+    password = input("enter your password:")
+    print("password should contain uppercase, lowercase and number")
