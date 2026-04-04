8af2f16b2c9799503127b4857544eb2d0c7c2e67
cleo581 <fclive7@gmail.com>
2026-04-04 19:56:35 +0100

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,10 @@
+input("enter a number")
+for i in range(1,10):
+    if i%2!=0:
+        print("odd numbers:","odd_numbers")
+        cfruits = ["apple", "banana", "cherry", "mango"]
+
+capitalized_fruits = [fruit.capitalize() for fruit in cfruits]
+
+print("Updated list:", capitalized_fruits)
+
