a10bae5b50530346dfd1df513291712805f73e97
cleo581 <fclive7@gmail.com>
2026-04-09 15:15:20 +0100

    Add files via upload

---
+++ defclass.py
@@ -0,0 +1,29 @@
+class dog:
+    species="canis lupus familiaris"
+    def __init__(self, name, age=0):
+        self.name = name
+        #class atritubit,specific to the class
+        self.age = age
+    
+    def bark(self, sound):
+        return self.name + " sound like " + sound
+    
+        def __str__(self):
+            return f"{self.name} is {self.age} years old"
+    
+    tiger=("tiger",15)
+    print(tiger)
+    bark=("auf")
+    print(bark)
+    dog2=("tom",15)
+    print(dog2)
+    bark2=("woof")
+    print(bark2)
+
+#a dog class example to understand object oriented programing
+class goldenretriver:
+    pass
+class husky:
+    pass
+class poodle:
+    pass
