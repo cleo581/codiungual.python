7a8ede3736310cf89f5c1a6e52747424f22905d5
cleo581 <fclive7@gmail.com>
2026-04-12 16:58:04 +0100

    Add files via upload

---
+++ stringuppercase.py
@@ -0,0 +1,15 @@
+class Stringuppercase():
+    def __init__(self):
+        self.string1=" "
+    def getstring(self):
+            self.string1=input("enter a string")
+    def printstring(self):
+         print("result is ",self.string1.upper())
+string1=Stringuppercase()
+string1.getstring()
+string1.printstring()
+
+
+        
+
+ 
