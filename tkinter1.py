e489eb0c6fc828b6f173f3b914329d0ef497ca1f
cleo581 <fclive7@gmail.com>
2026-05-27 19:07:04 +0100

    Add files via upload

---
+++ tkinter1.py
@@ -0,0 +1,18 @@
+# Import tkinter
+
+import tkinter as tk
+
+# Create Window
+
+window = tk.Tk()
+
+# Set window title and size
+
+window.title("my first gui screen")
+
+window.geometry("800x600")
+
+# Start GUI loop
+
+window.mainloop()
+
